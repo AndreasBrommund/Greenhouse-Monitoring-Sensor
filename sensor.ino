975170f70c3c50f509216f22a73c2c8eebdcbdc2
Andreas Brommund <andreas@brommund.se>
2018-08-16 16:31:58 +0200

    Restart the project, can read and print the temperature

--- sensor.ino
+++ sensor.ino
@@ -1,47 +1,88 @@
-#include <SoftwareSerial.h>
+#include "DHT.h"
 
-#define RX 10
-#define TX 11
+#define DHTPIN_INSIDE 2
+#define DHTPIN_OUTSIDE 3
+#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DEBUG 1
 
-String AP = "test";
-String PW = "test123456";
+DHT sensorInside(DHTPIN_INSIDE, DHTTYPE);
+DHT sensorOutside(DHTPIN_OUTSIDE, DHTTYPE);
 
-SoftwareSerial esp8266(RX,TX); 
- 
-  
+struct sensorData {
+  bool hasData;
+  float humidity;
+  float temperature;
+};
+
+struct climateData {
+  struct sensorData *inside; 
+  struct sensorData *outside; 
+};
+
 void setup() {
-  
-  Serial.begin(9600);
-  esp8266.begin(115200);
+  Serial.begin(9600); 
 
-  sendCommand("AT",5,(char *)"OK");
-  sendCommand("AT+CWMODE=1",5,(char *)"OK");
-  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PW + "\"",20,(char *)"OK");
+  sensorInside.begin();
+  sensorOutside.begin();
 }
+
 void loop() {
-  char c = esp8266.read();
+  climateData *data = readClimateData();
+  printClimateData(data);
+  delay(10000);
+}
 
-  if ((int)c != -1){
-    Serial.print(c);
+sensorData *readSensor(DHT dht){
+  sensorData *data = (sensorData*) malloc(sizeof(sensorData));
+  
+  data->humidity = dht.readHumidity();
+  data->temperature = dht.readTemperature();
+
+  if(isnan(data->humidity) || isnan(data->temperature)){
+    data->hasData = false;
+    return data;
+  } else {
+    data->hasData = true; 
+    return data; 
   }
 }
 
-bool sendCommand(String command, int timeOut, char replay[]){
-  int times = 0;
-  while(timeOut != 0){
-    Serial.print(++times);
-    Serial.println(" > " + command);
-    esp8266.println(command);
-    if(esp8266.find(replay)){
-      Serial.println("OK");
-      return true;
+climateData *readClimateData(){
+  climateData *data = (climateData*)malloc(sizeof(climateData));
+
+  data->inside = readSensor(sensorInside);
+  data->outside = readSensor(sensorOutside);
+  
+  #if (DEBUG==1)
+    if(!data->inside->hasData){
+      Serial.println("Error, reading from inside sensor!");
     }
-    timeOut--;
-  }
-  Serial.println("FAIL");
-  return false;
+    if(!data->outside->hasData){
+      Serial.println("Error, reading from outside sensor!");
+    }
+  #endif
+  
+  return data;
 }
 
+void printClimateData(climateData *data){  
+  Serial.println("----Inside----");
+  printSensorData(data->inside);
+  Serial.println("----Outside----");
+  printSensorData(data->outside);
+  Serial.println("###############");
+}
 
-
+void printSensorData(sensorData *data){
+  if(data->hasData){
+    Serial.print("Humidity: "); 
+    Serial.print(data->humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(data->temperature);
+    Serial.println(" *C");
+  } else {
+    Serial.println("No data!");
+  }
+}
 
